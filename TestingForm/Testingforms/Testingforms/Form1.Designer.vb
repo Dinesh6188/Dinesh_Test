adbc8f7deb4899a5995fc1b7a2fc983ccc53cb02
dinesh.s <dinesh.s@GBIC-DOTNET2-12>
2023-12-20 22:10:13 +0530

    label is added

--- TestingForm/Testingforms/Testingforms/Form1.Designer.vb
+++ TestingForm/Testingforms/Testingforms/Form1.Designer.vb
@@ -22,63 +22,51 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        TextBox1 = New TextBox()
         Label1 = New Label()
+        TextBox1 = New TextBox()
         Button1 = New Button()
-        Label2 = New Label()
         SuspendLayout()
-        ' 
-        ' TextBox1
-        ' 
-        TextBox1.Location = New Point(336, 79)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(241, 27)
-        TextBox1.TabIndex = 0
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(214, 85)
+        Label1.Location = New Point(136, 78)
         Label1.Name = "Label1"
         Label1.Size = New Size(49, 20)
-        Label1.TabIndex = 1
+        Label1.TabIndex = 0
         Label1.Text = "Name"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(213, 83)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(268, 27)
+        TextBox1.TabIndex = 1
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(306, 160)
+        Button1.Location = New Point(270, 167)
         Button1.Name = "Button1"
         Button1.Size = New Size(94, 29)
         Button1.TabIndex = 2
         Button1.Text = "Submit"
         Button1.UseVisualStyleBackColor = True
         ' 
-        ' Label2
-        ' 
-        Label2.AutoSize = True
-        Label2.Location = New Point(212, 19)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(53, 20)
-        Label2.TabIndex = 3
-        Label2.Text = "Label2"
-        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
-        Controls.Add(Label2)
         Controls.Add(Button1)
-        Controls.Add(Label1)
         Controls.Add(TextBox1)
+        Controls.Add(Label1)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
-    Friend WithEvents Label2 As Label
 End Class
